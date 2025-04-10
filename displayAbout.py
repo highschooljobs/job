4f4ca6b529d0863a164ecdec5f357ecdf44a7ea5
highschooljobs <myhighschooljobs@gmail.com>
2025-04-10 03:24:02 +0000

    added a paragraph about the website

--- displayAbout.py
+++ displayAbout.py
@@ -14,10 +14,15 @@ navigator = """
 <hr>
 """
 
-print("Content-type:text/html")
+
+print("Content-type:text/html; charset=utf-8\n")
 print("<html>")
+print("<head>")
+print("<meta charset='UTF-8'>")
+print("</head>")
 print("  <body>")
 print("    <h1>About</h1>")
 print(navigator)
+print("Finding a job as a teen isn’t easy — we know because we’ve been there. That’s why we created JobNest, a place dedicated to helping teens find real, up-to-date job opportunities. Whether you’re looking for your first part-time gig, a summer job, or a flexible position after school, we make it simple to discover who’s hiring and what you’ll need to apply. Our goal is to take the stress out of job hunting and help you take the first step toward building experience, earning money, and growing your future.")
 print("  </body>")
 print("</html>")
