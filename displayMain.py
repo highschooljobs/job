ede51c1e68282dcdc1d0c2df9f543f65fa148b5c
highschooljobs <myhighschooljobs@gmail.com>
2025-07-14 22:47:58 +0000

    fixing displayMain to origional file, with search bar and mobile view

--- displayMain.py
+++ displayMain.py
@@ -8,11 +8,11 @@ arguments = cgi.FieldStorage()
 citySelected = "cityState" in arguments
 city = arguments["cityState"].value if citySelected else ""
 
-# Check if user is on a phone
+#check if user is on a phone
 phone = "Phone" in os.environ["HTTP_USER_AGENT"]
 
 dbg_mode = "dbg" in arguments
-dbg = True if dbg_mode and arguments["dbg"].value == '1' else False
+dbg = True if dbg_mode and arguments["dbg"].value  == '1' else False
 
 jobpath = "/var/lib/db/jobs.db"
 
@@ -45,6 +45,7 @@ if citySelected:
     minLat = lat - 0.1
     maxLat = lat + 0.1
 
+
 # Execute SELECT statement
 inBox = '" OR (latitude > ' + str(minLat) + ' AND latitude < ' + str(maxLat) + ' AND longitude > ' + str(minLong) + ' AND longitude < ' + str(maxLong) + '))' if citySelected and lat is not None else ""
 addcity = ' AND (cityState = "' + city + inBox if citySelected and lat is not None else ""
@@ -55,6 +56,7 @@ if citySelected or dbg:
 else:
     jobsRaw = []
 
+
 jobs = []
 for row in jobsRaw:
     job = []
@@ -63,104 +65,113 @@ for row in jobsRaw:
         job.append(x)
     job.insert(0, str(round(distance, 1)))
     jobs.append(job)
-jobs.sort(key=lambda x: float(x[0]))
+jobs.sort(key=lambda x : float(x[0]))
 
 columns = ["distance", "company", "title", "id", "age", "pay", "address", "cityState", "latitude", "longitude", "url"]
 exclude = ["id", "longitude", "latitude"]
 
 conn.close()
 
+
+# change the style if your on mobile view vs computer
 if phone:
     style = """
     <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
     table {
      margin-top: 20px;
-     font-family: arial, sans-serif;
-     border-collapse: collapse;
-     width: 100%;
-     font-size: 140%;
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    font-size: 140%;
     }
 
-    body {
-     font-family: arial, sans-serif;
-     font-size: 200%;
+    body{
+    font-family: arial, sans-serif;
+    font-size: 200%;
     }
 
-    select {
-     font-family: arial, sans-serif;
-     font-size: 110%;
+    select, input{
+    font-family: arial, sans-serif;
+    font-size: 110%;
     }
 
     td, th {
-     border: 1px solid #dddddd;
-     text-align: left;
-     padding: 18px;
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 18px;
     }
 
     td:nth-child(2) {
-     max-width: 300px;
-     word-wrap: break-word;
+    max-width: 300px;
+    word-wrap: break-word;
     }
 
     tr:nth-child(even) {
-     background-color: #dddddd;
+    background-color: #dddddd;
+    }
+    button i {
+    font-size: 20px;
+    }
+    button {
+    background: none;
+    border: none;
+    cursor: pointer;
     }
     </style>
     </head>
     """
 else:
-    style = """
-    <head>
-    <style>
-    table {
-     margin-top: 20px;
-     font-family: arial, sans-serif;
-     border-collapse: collapse;
-     width: 100%;
-    }
+     style = """
+        <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <style>
+        table {
+        margin-top: 20px;
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+        }
 
-    body {
-     font-family: arial, sans-serif;
-    }
+        body{
+        font-family: arial, sans-serif;
+        }
 
-    select {
-     font-family: arial, sans-serif;
-     font-size: 110%;
-    }
+        select, input{
+        font-family: arial, sans-serif;
+        font-size: 110%;
+        }
 
-    td, th {
-     border: 1px solid #dddddd;
-     text-align: left;
-     padding: 8px;
-    }
+        td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+        }
 
-    td:nth-child(2) {
-     max-width: 300px;
-     word-wrap: break-word;
-    }
+        td:nth-child(2) {
+        max-width: 300px;
+        word-wrap: break-word;
+        }
 
-    tr:nth-child(even) {
-     background-color: #dddddd;
-    }
-
-    .centered-select {
-     text-align: center;
-     margin-top: 20px;
-     margin-bottom: 10px;
-    }
-
-    .centered-select select {
-     font-size: 1.1em;
-     padding: 6px;
-    }
-    tr:hover {
-    background-color: #eef2f7;
-    transition: background-color 0.2s ease-in-out;
-    }
-    </style>
-    </head>
-    """
+        tr:nth-child(even) {
+        background-color: #dddddd;
+        }
+        tr:hover {
+        background-color: #eef2f7;
+        transition: background-color 0.2s ease-in-out;
+        }
+        button i {
+        font-size: 20px;
+        }
+        button {
+        background: none;
+        border: none;
+        cursor: pointer;
+        }
+        </style>
+        </head>
+        """
 
 navigator = """
 <hr>
@@ -170,18 +181,22 @@ navigator = """
     font-weight: normal;
     color: black;
   }
+
   .nav-link:hover {
     font-weight: bold;
   }
+
   .active {
     font-weight: bold;
     color: black;
     text-decoration: none;
   }
+
   .nav-container {
     text-align: center;
   }
 </style>
+
 <div class="nav-container">
   <p>
     <a href="index.html" class="active">Home</a> |
@@ -190,19 +205,18 @@ navigator = """
 </div>
 <hr>
 """
-
-print("Content-type:text/html\n")
+print("Content-type:text/html")
 print(style)
-print("<body>")
-print('<h1 style="text-align: center;">mangohub</h1>')
-print('<p style="text-align: center;">Find jobs for highschool teens</p>')
+print("  <body>")
+print('    <h1 style="text-align: center;">mangohub</h1>')
+print('    <p style="text-align: center;">Find jobs for highschool teens</p>')
 print(navigator)
-
 if dbg:
-    print("City Selected: " + str(citySelected) + "<br>")
-    print("City: '" + city + "'<br>")
+    print("City Selected: " + str(citySelected) +  "</br>")
+    print("City: '" + city + "'</br>")
     for i in arguments.keys():
-        print(i + " '" + arguments[i].value + "'<br>")
+        print(i + " '" +  arguments[i].value + "'</br>")
+
 
 # Search bar with Enter key support
 print('''
@@ -214,7 +228,7 @@ print('''
     placeholder="Start typing..."
     onkeydown="handleKey(event)"
   />
-  <button onclick="useCurrentLocation()">Use Current Location</button>
+  <button onclick="useCurrentLocation()" title="Use Current Location"><i class="fas fa-location-crosshairs"></i></button>
   <datalist id="cities">
 ''')
 
@@ -262,37 +276,77 @@ function handleKey(event) {
 </script>
 ''')
 
+# PRINT TABLE
 # JOBS TABLE
 if not jobs and not dbg:
     print("<p style='text-align: center; margin-top: 20px;'>Please select a city to view jobs.</p>")
 if jobs or dbg:
     print("    <table>")
     print("      <tr>")
-    for col in columns:
-        if col not in exclude:
-            print(f"        <th>{col.capitalize()}</th>")
-    print("      </tr>")
+    if dbg:
+        print("        <th>Distance (mi)</th>")
+        print("        <th>Company</th>")
+        print("        <th>Title</th>")
+        print("        <th>Id</th>")
+        print("        <th>Age</th>")
+        print("        <th>Pay</th>")
+        print("        <th>Address</th>")
+        print("        <th>cityState</th>")
+        print("        <th>Latitude</th>")
+        print("        <th>Longitude</th>")
+        print("        <th>url</th>")
+    else:
+        if phone:
+            print("<th> Jobs </th>")
+        else:
+            print("        <th>Distance (mi)</th>")
+            print("        <th>Company</th>")
+            print("        <th>Title</th>")
+            print("        <th>Age</th>")
+            print("        <th>Pay</th>")
+            print("        <th>Address</th>")
+            print("        <th>cityState</th>")
+            print("        <th>url</th>")
 
-    for i in jobs:
-        print("      <tr>")
-        for idx, value in enumerate(i):
-            if columns[idx] not in exclude:
-                if value == i[0]:
-                    print("<td style='text-align: center;'>" + str(value) + "</td>")
+    print("      </tr>")
+    if dbg:
+        for i in jobs:
+            print("      <tr>")
+            for x in i:
+                if x == i[0]:
+                    print("<td style='text-align: center;'>" + str(x) + "</td>")
                 else:
-                    print("<td>" + str(value) + "</td>")
-        print("      </tr>")
+                    print("<td>" + str(x) + "</td>")
+            print("      </tr>")
+    else:
+        if phone:
+            for job in jobs:
+                print("<tr>")
+                print("<td>")
+                print(job[1] + ", " +  job[2] + "<br> ")
+                print(job[6] + ", " + str(job[7]) + "<br>")
+                print(job[0] + " mi, "  + job[4] + "yo, " + job[5]  +  " " + job[10])
+                print("<td>")
+                print("</tr>")
+        else:
+            for i in jobs:
+                print("      <tr>")
+                for idx, value in enumerate(i):
+                    if columns[idx] not in exclude:
+                        if value == i[0]:
+                            print("<td style='text-align: center;'>" + str(value) + "</td>")
+                        else:
+                            print("<td>" + str(value) + "</td>")
+                print("      </tr>")
 
     print("    </table>")
-
 if dbg:
     print("Total jobs: ", len(jobs), "<br>")
     print(jobs)
-    print("<font size=+1>Environment</font><br>")
+    print ("<font size=+1>Environment</font><br>")
     for param in os.environ.keys():
         print("<b>%20s</b>: %s<br>" % (param, os.environ[param]))
     print(lat, long, "<br>")
     print(select, "<br>")
 
-print("</body>")
-
+print("  </body>")
